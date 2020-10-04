b5900f7a671328955b134f00d971216c2a3389f9
GeoffreyMuasya <gmuasya2@gmail.com>
2020-10-04 01:03:28 -0700

    Check my code

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -35,6 +35,8 @@ public class InfoActivity extends AppCompatActivity {
     }
     public void createPhoneIntent(View view){
         Intent dial=new Intent(Intent.ACTION_DIAL);
+        Uri phoneUri=Uri.parse("tel:0123456789");
+        dial.setData(phoneUri);
     }
 
 
